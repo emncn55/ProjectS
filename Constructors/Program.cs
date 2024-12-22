eef3a661d02f6344a6c3b4dff315e4c003aafed7
emncn55 <canemin0@hotmail.com>
2024-12-22 23:01:50 +0300

    Constructors
    C#

---
+++ Constructors/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+class Constructors
+{
+    static void Main()
+    {
+        CustomerManager customerManager = new CustomerManager(20);
+        //CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.List();
+
+        Product product = new Product("Emin", 55);
+        product.Read();
+
+        EmployeeManager employeeManager = new EmployeeManager(new DatabaseLogger());
+        employeeManager.Add();
+
+        PersonManager personManager = new PersonManager("Emin");
+        personManager.Add();
+
+        Teacher.Number = 10; // static class
+        Console.WriteLine($"Number is: {Teacher.Number}");
+        Utilities.Validate(); // -> "Validation is done"
+
+
+
+
+
+        Console.ReadLine();
+    }
+}
+class CustomerManager
+{
+    int _count;
+    public CustomerManager(int count) // Constructor(yapıcı), ihtiyaç duyulan parametleri almayı sağlar.
+    {
+        _count = count;
+    }
+    public CustomerManager() // overloading yapılabilir.
+    {
+
+    }
+
+    public void List()
+    {
+        Console.WriteLine($"Listed! {_count} people");
+    }
+    public void Add()
+    {
+        Console.WriteLine($"Added! {_count} people");
+    }
+
+}
+class Product
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    private string _name;
+    private int _id;
+
+
+    public Product(string name, int id)
+    {
+        _name = name;
+        _id = id;
+    }
+
+    public void Read()
+    {
+        Console.WriteLine($"Name is: {_name}, Id is: {_id}");
+    }
+}
+interface ILogger
+{
+    void Log();
+}
+class DatabaseLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to Database!");
+    }
+}
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        Console.WriteLine("Logged to File");
+    }
+}
+class EmployeeManager
+{
+    private ILogger _logger;
+
+    
+    public EmployeeManager(ILogger logger)
+    {
+        _logger = logger;
+
+    }
+
+    public void Add()
+    {
+        _logger.Log();
+        Console.WriteLine("Added!");
+    }
+}
+class BaseClass
+{
+    private string _entity;
+    public BaseClass(string entity)
+    {
+        _entity = entity;
+    }
+    public void Message()
+    {
+        Console.WriteLine($"{_entity} message");
+    }
+}
+class PersonManager : BaseClass
+{
+    public PersonManager(string entity):base(entity) //base sınıfa yani "BaseClass" classına entity değerini gönderir.Constructor ile!!
+    {
+
+    }
+    public void Add()
+    {
+        Console.WriteLine("Added!");
+        Message();
+    }
+}
+static class Teacher // static bir sınıfın herşeyi static olmak zorundadır.
+{
+    public static int Number { get; set; }
+    
+}
+static class Utilities // static bir sınıfın herşeyi static olmak zorundadır.
+{
+    public static void Validate()
+    {
+        Console.WriteLine("Validation is done");
+    }
+}
---
+++ Constructors/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Constructors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Constructors")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("2a43178b-50a4-431b-bdb7-1651ff56bd4c")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
