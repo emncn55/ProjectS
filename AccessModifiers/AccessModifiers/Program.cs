77cf0514102e0e972807f6219ffd087ec3d885d7
emncn55 <canemin0@hotmail.com>
2024-12-22 22:59:51 +0300

    AccessModifiers
    cSharp

---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class AccessModifiers
+{
+    static void Main()
+    {
+
+    }
+}
+class Customer
+{
+    //private int id { get; set; } sadece tanımlandığı blokta kullanılabilir(Customer)
+    //protected int Id { get; set; } tanımlandığı blokta ve inherit edildiği blokta kullanılabilir(Customer,Student)
+    public void Delete()
+    {
+
+    }
+    public void Save()
+    {
+
+    }
+
+}
+class Student : Customer
+{
+    public void Save2()
+    {
+        Customer customer = new Customer();
+    }
+}
+internal class Course  // bağlı olduğu proje içinde bu projeyi referans ihtiyacı olmadan kullanabilirsiniz internal sayesinde.
+{
+    public string Name { get; set; }
+
+}
+public class Asadmlaskmclas // proje dışında başka projelerde de kullanabilmek için diğer projeye using AccessModifiers eklenmelidir ve class public yapılmalıdır.
+{
+
+}
