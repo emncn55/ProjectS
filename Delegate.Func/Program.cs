3300cf3d3cac0a99a47786b3d189e5d7e8c7ed6b
emncn55 <canemin0@hotmail.com>
2024-12-30 18:09:35 +0300

    Deletegate-Events
    C#

---
+++ Delegate.Func/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Delegate.Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+            Func<int> getRandomNumber2=()=>new Random().Next(1, 100);
+
+            Console.WriteLine(getRandomNumber());
+            Thread.Sleep(1000);
+            Console.WriteLine(getRandomNumber2());
+            Console.ReadLine();
+        }
+    }
+}
